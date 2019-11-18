f29b71db5e026f5151790b5da8f29fef803a3c0e
figosun <27018790@qq.com>
2019-11-18 14:45:19 +0800

    Update sf-binance.py

--- tests/sf-binance.py
+++ tests/sf-binance.py
@@ -4,3 +4,4 @@ api_secret = "vHC9cnCEIYq6qRQQRPWqOreGs4PpGF2JrAClezhnZKidDd3R28jLvNuP4foRW3jo"
 client = Client(api_key, api_secret)
 
 klines = client.get_historical_klines("BTCUSDT", Client.KLINE_INTERVAL_1DAY, "1 Sep, 2019", "30 Oct, 2019")
+print (klines)
